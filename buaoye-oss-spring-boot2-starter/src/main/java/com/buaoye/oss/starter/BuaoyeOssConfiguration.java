b1473894b5614eef22e9914bcab89c16dad716c3
Jayson Wu <catalpawoo524@gmail.com>
2024-12-18 11:11:33 +0800

    feat(SpringBoot2): SpringBoot2入口实现，最低支持版本为2.7.0

---
+++ buaoye-oss-spring-boot2-starter/src/main/java/com/buaoye/oss/starter/BuaoyeOssConfiguration.java
@@ -0,0 +1,43 @@
+package com.buaoye.oss.starter;
+
+import com.buaoye.oss.core.cache.BayOssCacheTask;
+import com.buaoye.oss.core.cache.property.BayOssCacheProperty;
+import com.buaoye.oss.core.client.BayOssClientManager;
+import com.buaoye.oss.core.client.property.BayOssClientProperty;
+import com.buaoye.oss.core.handler.BayOssHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 自动装配配置类
+ *
+ * @author Jayson Wu
+ * @since 2024-12-16
+ */
+@Configuration
+@EnableScheduling
+@EnableConfigurationProperties({
+        BayOssCacheProperty.class,
+        BayOssClientProperty.class
+})
+public class BuaoyeOssConfiguration {
+
+    @Bean
+    public BayOssClientManager bayOssClientManager(@Autowired BayOssClientProperty bayOssClientProperty) {
+        return new BayOssClientManager(bayOssClientProperty);
+    }
+
+    @Bean
+    public BayOssHandler bayOssHandler() {
+        return new BayOssHandler();
+    }
+
+    @Bean
+    public BayOssCacheTask bayOssCacheTask(@Autowired BayOssCacheProperty bayOssCacheProperty) {
+        return new BayOssCacheTask(bayOssCacheProperty);
+    }
+
+}
